23383c993bf0cdfadd16130d75df964c857508ef
Kartik Singh <96460163+toof04@users.noreply.github.com>
2026-06-12 02:42:27 +0530

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int ans = 0;
+
+        int left = 0;
+        int right= people.size()-1;
+
+        while(left<=right){
+            if(people[left]+people[right]<=limit){
+                left++;
+                right--;
+            }
+            else{
+                right--;
+            }
+            ans++;
+        }
+        return ans;
+
+    }
+};
