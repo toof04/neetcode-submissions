1c1e2c92ba59e14bf93d257e4359e159275bdeaa
Kartik Singh <96460163+toof04@users.noreply.github.com>
2026-06-17 21:45:43 +0530

    Add: decode-string - submission-0

---
+++ Data Structures & Algorithms/decode-string/submission-0.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string decodeString(string str) {
+     stack<string>s;
+     stack<int>n;
+
+     
+        int num = 0;
+        string current = "";
+        for(char st : str){
+            if(isdigit(st)){
+                 num = num*10 + (st - '0');
+            }
+            else if( st == '['){
+                n.push(num);
+                s.push(current);
+                num = 0;
+                current = ""; 
+            }
+            else if (st==']'){
+                int repeat = n.top();
+                string prev = s.top();
+                n.pop();
+                s.pop();
+                string temp = "";
+                for(int i = 0; i< repeat; i++){
+                    temp+=current;
+                }
+                current = prev + temp;
+
+            } else {
+                current += st;
+            }
+
+
+            }
+
+
+        return current;                                                                                
+    }
+};
