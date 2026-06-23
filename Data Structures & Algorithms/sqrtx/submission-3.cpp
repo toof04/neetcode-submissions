a76fa7d5266b9daa5d315da6a2b275960140f936
Kartik Singh <96460163+toof04@users.noreply.github.com>
2026-06-23 18:55:09 +0530

    Add: sqrtx - submission-3

---
+++ Data Structures & Algorithms/sqrtx/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int left = 1;
+        int right  = x/2;
+        long mid;
+        if(x<2)return x;
+        while(left<=right){
+             mid = left + (right-left)/2;
+            if (mid*mid==x)return mid;
+            else if(mid*mid<x)left = mid+1;
+            else right = mid-1;
+        } 
+        return right;
+    }
+};
