7dc1dc0959ee0f32e8c49d918106647b2ccd4206
Kartik Singh <96460163+toof04@users.noreply.github.com>
2026-06-26 14:20:12 +0530

    Add: redundant-connection - submission-1

---
+++ Data Structures & Algorithms/redundant-connection/submission-1.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+
+    int find(vector<int>&roots, int x){
+        if (roots[x]==x){
+            return x;
+        }
+        return roots[x]=find(roots,roots[x]);
+
+
+    }
+    bool unite(vector<int>&roots, int x, int y){
+        x = find(roots,x);
+        y = find(roots,y);
+        if (x!=y){
+            roots[x]=y;
+            return true;
+        }
+        return false;
+
+    }
+
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        vector<int>roots(n+1);
+        for(int i = 0; i<n; i++){
+            roots[i]=i;
+        }
+        vector<int>ans(2,0);
+        for(auto edg : edges){
+            int x = find(roots,edg[0]);
+            int y = find(roots,edg[1]);
+            if (!unite(roots,x,y)){
+                ans[0]=edg[0];
+                ans[1]=edg[1];
+            }
+        }
+
+        return ans;
+    }
+};
