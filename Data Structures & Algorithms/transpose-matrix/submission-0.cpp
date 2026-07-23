f674946cd1bcfb890bb76cda8edbba3e74a92707
Kartik Singh <96460163+toof04@users.noreply.github.com>
2026-07-23 20:04:23 +0530

    Add: transpose-matrix - submission-0

---
+++ Data Structures & Algorithms/transpose-matrix/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        vector<vector<int>>ans;
+        for(int i = 0; i<matrix[0].size(); i++){
+            vector<int>newcol;
+            for(int j=0; j < matrix.size(); j++){
+                newcol.push_back(matrix[j][i]);
+            }
+            ans.push_back(newcol);
+        }
+        return ans;
+    }
+};
