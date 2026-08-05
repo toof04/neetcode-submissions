3e4905d278be813d6f5c5a315581a0fa27b231b3
Kartik Singh <96460163+toof04@users.noreply.github.com>
2026-08-06 01:26:50 +0530

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    void traverse(int start,int n, int k, vector<vector<int>>&ans, vector<int>&temp){
+        if(temp.size()==k){
+            ans.push_back(temp);
+            return;
+        }
+        for(int i = start; i <= n; i++){
+            temp.push_back(i);
+            traverse(i+1,n,k,ans,temp);
+            temp.pop_back();
+        }
+    }
+
+
+
+
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>>ans;
+        vector<int>temp;
+        traverse(1,n,k,ans,temp);
+        return ans;
+    }
+};
